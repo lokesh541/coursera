eb1903533f657e360aedef27b671f90b0f42f701
lokesh541 <lrbeeram@gmail.com>
2019-02-07 00:09:28 +0530

    added regex to ensure numerica input

--- guessinggame.sh
+++ guessinggame.sh
@@ -2,19 +2,26 @@ function guess() {
   echo "how many files are in the current directory?"
   read response
 }
-no_of_files=$(ls | wc -l)
+no_of_files=$(ls -a | wc -l)
 guess
 
-while [[ $response -ne $no_of_files ]]; 
+while [[ $response -ne $no_of_files ]];
 do
-  if [[ $response -lt $no_of_files ]]; 
-  then
-    echo "too low, guess again"
-    guess
+  if [[ $response = ^[0-9]+$ ]]; then
+    #statements
+    if [[ $response -lt $no_of_files ]];
+    then
+      echo "too low, guess again"
+      guess
+    else
+      echo "too High, guess again"
+      guess
+    fi
   else
-    echo "too High, guess again"
+    echo "not a number, please enter an integer"
     guess
   fi
+
 
 done
 echo "you win"
